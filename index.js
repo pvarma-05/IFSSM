79c832521ae52c52a41f040348199c5d2228ac47
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2024-09-09 22:05:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 let loginCheck = false;
 
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {

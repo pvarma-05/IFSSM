3e20a6c3d2737cd61242b51465a87a6f0d0c1e95
pvarma 05 <pradeeppenumatsa555@gmail.com>
2024-09-09 21:33:47 +0530

    Added Files

---
+++ script.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { log } = require("console");
+const app = express();
+let loginCheck = false;
+
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    if (loginCheck === false)
+        res.sendFile(__dirname + "/login.html");
+    else
+        res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/logout",(req,res)=>{
+    loginCheck = false;
+    res.redirect("/");
+});
+
+app.post("/", (req, res) => {
+    const user = req.body["username"];
+    const pass = req.body["password"];
+    
+    if (user === "devi_05" && pass === "devi_05") {
+        loginCheck = true;
+        res.sendFile(__dirname + "/index.html");
+    } else {
+        res.sendFile(__dirname + "/login.html");
+    }
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
